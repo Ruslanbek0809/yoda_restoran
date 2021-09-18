ec8cac6c176c544b351bf984deacb5e1b5655cbf
Ruslanbek0809 <ruslanbek0809@gmail.com>
2021-09-18 04:59:08 -0700

    svg and icon added to assets folder

---
+++ lib/widgets/custom_icon_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:yoda_res/utils/utils.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomIconButton extends StatelessWidget {
+  final IconData iconData;
+  final Function onTap;
+  const CustomIconButton({
+    required this.iconData,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: AppTheme.WHITE,
+      shape: CircleBorder(),
+      elevation: 5,
+      child: InkWell(
+        customBorder: CircleBorder(),
+        onTap: onTap as void Function()?,
+        child: Padding(
+          padding: EdgeInsets.all(8.w),
+          child: Icon(
+            iconData,
+            size: 27.w,
+            color: AppTheme.BLACK,
+          ),
+        ),
+      ),
+    );
+  }
+}
