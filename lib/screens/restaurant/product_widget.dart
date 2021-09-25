6a1c11ddeb0ae2856be972b9764d6b24b7499ac9
Ruslanbek0809 <ruslanbek0809@gmail.com>
2021-09-25 11:28:23 -0700

    untracked files added to previous work

---
+++ lib/screens/restaurant/product_widget.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:yoda_res/models/food_model.dart';
+import 'package:yoda_res/screens/restaurant/product_bottom_sheet.dart';
+import 'package:yoda_res/utils/utils.dart';
+import 'package:yoda_res/widgets/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProductWidget extends StatefulWidget {
+  final FoodModel food;
+  const ProductWidget({Key? key, required this.food}) : super(key: key);
+
+  @override
+  _ProductWidgetState createState() => _ProductWidgetState();
+}
+
+class _ProductWidgetState extends State<ProductWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _tweenController;
+  Tween<double> _tween = Tween(begin: 1, end: 0.98);
+
+  late FoodModel food;
+  bool isButtonToggled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    food = widget.food;
+//// Container bounce back
+    _tweenController = AnimationController(
+        duration: const Duration(milliseconds: 50), vsync: this)
+      ..addStatusListener((status) {
+//// This listener was used to repeat animation once
+        if (status == AnimationStatus.completed) {
+          _tweenController.reverse();
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tweenController.dispose();
+  }
+
+  void _onProductBottomSheetClicked(FoodModel food) {
+    showProductBottomSheet(context, food);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _tween.animate(
+          CurvedAnimation(parent: _tweenController, curve: Curves.bounceInOut)),
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppTheme.MAIN_LIGHT,
+          borderRadius: AppTheme().mainBorderRadius,
+        ),
+        padding: EdgeInsets.all(5.w),
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                YodaImage(
+                  image: food.image,
+                  height: constraints.maxWidth,
+                  width: constraints.maxWidth,
+                  borderRadius: 20.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.w, bottom: 4.w),
+                  child: Text(
+                    food.name,
+                    style: TextStyle(
+                      fontSize: 17.sp,
+                      color: AppTheme.FONT_COLOR,
+                    ),
+                  ),
+                ),
+                Text(
+                  '${food.weight} ${food.weightType}',
+                  style: TextStyle(
+                    fontSize: 15.sp,
+                    color: AppTheme.DRAWER_ICON,
+                  ),
+                ),
+                Spacer(),
+//// Button Widget
+                AnimatedSwitcher(
+                  duration: Duration(milliseconds: 300),
+                  child: isButtonToggled
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Material(
+                              color: AppTheme.WHITE,
+                              borderRadius: AppTheme().buttonBorderRadius,
+                              elevation: 1,
+                              child: InkWell(
+                                borderRadius: AppTheme().buttonBorderRadius,
+                                onTap: () async {
+                                  _tweenController.forward();
+                                  if (isButtonToggled)
+                                    setState(() {
+                                      isButtonToggled = !isButtonToggled;
+                                    });
+                                },
+                                child: Padding(
+                                  padding: EdgeInsets.all(10.w),
+                                  child: Icon(
+                                    Icons.remove,
+                                    size: 25.w,
+                                    color: AppTheme.FONT_COLOR,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              '1',
+                              style: TextStyle(
+                                fontSize: 20.sp,
+                                color: AppTheme.FONT_COLOR,
+                              ),
+                            ),
+                            Material(
+                              color: AppTheme.WHITE,
+                              borderRadius: AppTheme().buttonBorderRadius,
+                              elevation: 1,
+                              child: InkWell(
+                                borderRadius: AppTheme().buttonBorderRadius,
+                                onTap: () {
+                                  _onProductBottomSheetClicked(food);
+                                },
+                                child: Padding(
+                                  padding: EdgeInsets.all(10.w),
+                                  child: Icon(
+                                    Icons.add,
+                                    size: 25.w,
+                                    color: AppTheme.FONT_COLOR,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      : Material(
+                          color: Colors.transparent,
+                          borderRadius: AppTheme().buttonBorderRadius,
+                          elevation: 1,
+                          child: InkWell(
+                            borderRadius: AppTheme().buttonBorderRadius,
+                            onTap: () async {
+                              _tweenController.forward();
+                              if (!isButtonToggled)
+                                setState(() {
+                                  isButtonToggled = !isButtonToggled;
+                                });
+                            },
+                            child: Ink(
+                              width: constraints.maxWidth,
+                              decoration: BoxDecoration(
+                                color: AppTheme.WHITE,
+                                borderRadius: AppTheme().buttonBorderRadius,
+                              ),
+                              padding: EdgeInsets.symmetric(vertical: 10.w),
+                              child: Text(
+                                '${food.price} TMT',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 20.sp,
+                                  color: AppTheme.FONT_COLOR,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                )
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
