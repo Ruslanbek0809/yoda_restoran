259e8db16254326dff42753069f9d7fef87b7f81
Ruslanbek0809 <ruslanbek0809@gmail.com>
2021-09-25 10:56:24 -0700

    CustomOutlinedButton added

---
+++ lib/widgets/custom_outlined_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../utils/utils.dart';
+
+class CustomOutlinedButton extends StatelessWidget {
+  final double? height;
+  final double? width;
+  final Color? color;
+  final String? text;
+  final double? borderRadius;
+  final bool isLoading;
+  final Function? onPressed;
+  CustomOutlinedButton(
+      {this.height,
+      this.width,
+      this.color,
+      this.text,
+      this.borderRadius,
+      this.isLoading = false,
+      this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        primary: color ?? AppTheme.MAIN,
+        onSurface: color ?? AppTheme.MAIN,
+        shadowColor: color ?? AppTheme.MAIN,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+            borderRadius: borderRadius as BorderRadiusGeometry? ??
+                AppTheme().buttonBorderRadius),
+        // minimumSize: Size(width!, height!),
+      ),
+      onPressed: onPressed as void Function()?,
+      child: isLoading
+          ? Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: CircularProgressIndicator(backgroundColor: AppTheme.WHITE),
+            )
+          : Text(
+              text!,
+              style: TextStyle(
+                color: AppTheme.WHITE,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+    );
+  }
+}
