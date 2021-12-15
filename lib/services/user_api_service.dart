18e3325bf45a85b8773edf394746ed59822cb553
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2021-12-15 12:42:10 +0500

    Initialised part I for LoginViewModel

--- lib/services/user_api_service.dart
+++ lib/services/user_api_service.dart
@@ -1,3 +1,42 @@
+import 'package:yoda_res/app/app.locator.dart';
+import 'package:yoda_res/app/app.logger.dart';
+import 'package:yoda_res/services/services.dart';
+
 class UserApiService {
-  
+  final log = getLogger('UserApiService');
+
+  final _apiRoot = locator<ApiRootService>();
+
+  // User? _currentUser;
+
+  // User get currentUser => _currentUser!;
+
+  // bool get hasLoggedInUser => _firebaseAuthenticationService.hasUser;
+
+  // Future<void> syncUserAccount() async {
+  //   final firebaseUserId =
+  //       _firebaseAuthenticationService.firebaseAuth.currentUser!.uid;
+
+  //   log.v('Sync user $firebaseUserId');
+
+  //   final userAccount = await _firestoreApi.getUser(userId: firebaseUserId);
+
+  //   if (userAccount != null) {
+  //     log.v('User account exists. Save as _currentUser');
+  //     _currentUser = userAccount;
+  //   }
+  // }
+
+  // Future<void> syncOrCreateUserAccount({required User user}) async {
+  //   log.i('user:$user');
+
+  //   await syncUserAccount();
+
+  //   if (_currentUser == null) {
+  //     log.v('We have no user account. Create a new user ...');
+  //     await _firestoreApi.createUser(user: user);
+  //     _currentUser = user;
+  //     log.v('_currentUser has been saved');
+  //   }
+  // }
 }
--- lib/ui/profile/login/login_view_model.dart
+++ lib/ui/profile/login/login_view_model.dart
@@ -1,3 +1,21 @@
-import 'package:stacked/stacked.dart';
+import 'package:yoda_res/app/app.locator.dart';
+import 'package:yoda_res/app/app.router.dart';
+import 'package:yoda_res/services/services.dart';
+import 'package:yoda_res/ui/profile/login_otp_view_model.dart';
 
-class LoginViewModel extends BaseViewModel {}
+class LoginViewModel extends LoginOtpViewModel {
+  LoginViewModel() : super(successRoute: Routes.homeView);
+
+  final _userApiService =
+      locator<UserApiService>();
+
+  @override
+  Future runLoginOtp() =>
+      _firebaseAuthenticationService.loginWithEmail(
+        email: emailValue,
+        password: passwordValue,
+      );
+
+  void navigateToCreateAccount() =>
+      navigationService.navigateTo(Routes.createAccountView);
+}
