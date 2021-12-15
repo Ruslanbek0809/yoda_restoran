b49346ff8d2b29f39f3cf8cbbad43b960cb58dd8
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2021-12-15 15:05:11 +0500

    Configured all needed pre setup for fcm

--- lib/ui/profile/login/login_view_model.dart
+++ lib/ui/profile/login/login_view_model.dart
@@ -1,21 +1,25 @@
+import 'package:stacked/stacked.dart';
 import 'package:yoda_res/app/app.locator.dart';
 import 'package:yoda_res/app/app.router.dart';
 import 'package:yoda_res/services/services.dart';
 import 'package:yoda_res/ui/profile/login_otp_view_model.dart';
 
-class LoginViewModel extends LoginOtpViewModel {
-  LoginViewModel() : super(successRoute: Routes.homeView);
+class LoginViewModel extends BaseViewModel {
+  // LoginViewModel() : super(successRoute: Routes.homeView);
 
-  final _userApiService =
-      locator<UserApiService>();
+  // final _userApiService = locator<UserApiService>();
 
-  @override
-  Future runLoginOtp() =>
-      _firebaseAuthenticationService.loginWithEmail(
-        email: emailValue,
-        password: passwordValue,
-      );
+  // @override
+  // Future runLoginOtp() {
+  //   return null;
+  // }
+  // // @override
+  // // Future runLoginOtp() =>
+  // //     _firebaseAuthenticationService.loginWithEmail(
+  // //       email: emailValue,
+  // //       password: passwordValue,
+  // //     );
 
-  void navigateToCreateAccount() =>
-      navigationService.navigateTo(Routes.createAccountView);
+  // void navigateToCreateAccount() =>
+  //     navigationService.navigateTo(Routes.createAccountView);
 }
