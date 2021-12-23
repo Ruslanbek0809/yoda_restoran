f30bb0bda5dc349b5868914d776f301539709f26
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2021-12-23 18:31:55 +0500

    Created clearCart() func in CartVM

--- lib/ui/cart/cart_view_model.dart
+++ lib/ui/cart/cart_view_model.dart
@@ -1,3 +1,18 @@
 import 'package:stacked/stacked.dart';
+import 'package:yoda_res/app/app.locator.dart';
+import 'package:yoda_res/app/app.logger.dart';
+import 'package:yoda_res/services/services.dart';
 
-class CartViewModel extends BaseViewModel {}
+class CartViewModel extends BaseViewModel {
+  final log = getLogger('CartViewModel');
+
+  final _hiveDbService = locator<HiveDbService>();
+
+  /// CLEAR CART
+  Future<void> clearCart() async {
+    log.i('clearCart()');
+
+    await _hiveDbService.clearCart();
+    notifyListeners();
+  }
+}
