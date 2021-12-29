2eb9f64cc7b720100c102a1a9a05d79e9ec4ad1b
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2021-12-29 09:52:32 +0500

    Generated HiveUser model

---
+++ lib/models/hive_models/hive_user.dart
@@ -0,0 +1,37 @@
+import 'package:hive/hive.dart';
+
+part 'hive_user.g.dart';
+
+@HiveType(typeId: 3)
+class HiveUser {
+  HiveUser({
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.mobile,
+    this.gender,
+    this.birthDate,
+  });
+
+  @HiveField(0)
+  final int? id;
+
+  @HiveField(1)
+  final String? firstName;
+
+  @HiveField(2)
+  final String? lastName;
+
+  @HiveField(3)
+  final String? email;
+
+  @HiveField(4)
+  final String? mobile;
+
+  @HiveField(5)
+  final String? gender;
+
+  @HiveField(6)
+  final String? birthDate;
+}
---
+++ lib/models/hive_models/hive_user.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hive_user.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class HiveUserAdapter extends TypeAdapter<HiveUser> {
+  @override
+  final int typeId = 3;
+
+  @override
+  HiveUser read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return HiveUser(
+      id: fields[0] as int?,
+      firstName: fields[1] as String?,
+      lastName: fields[2] as String?,
+      email: fields[3] as String?,
+      mobile: fields[4] as String?,
+      gender: fields[5] as String?,
+      birthDate: fields[6] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, HiveUser obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.firstName)
+      ..writeByte(2)
+      ..write(obj.lastName)
+      ..writeByte(3)
+      ..write(obj.email)
+      ..writeByte(4)
+      ..write(obj.mobile)
+      ..writeByte(5)
+      ..write(obj.gender)
+      ..writeByte(6)
+      ..write(obj.birthDate);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HiveUserAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
