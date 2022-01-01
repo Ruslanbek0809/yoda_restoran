7cbcb7f17c25a93066b81a016a22172f230622aa
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2022-01-01 12:17:43 +0500

    Made CheckoutService reactive and assigned new parmentType value

--- lib/services/checkout_service.dart
+++ lib/services/checkout_service.dart
@@ -1,19 +1,33 @@
+import 'package:stacked/stacked.dart';
 import 'package:yoda_res/app/app.locator.dart';
 import 'package:yoda_res/app/app.logger.dart';
 import 'package:yoda_res/models/models.dart';
+import 'package:yoda_res/utils/util_functions.dart';
 
 import 'api_service.dart';
 import 'hv_db_service.dart';
 
-class CheckoutService {
-  final log = getLogger('CartService');
+class CheckoutService with ReactiveServiceMixin {
+  final log = getLogger('CheckoutService');
+
+  CheckoutService() {
+    // 3
+    listenToReactiveValues([_paymentType]);
+  }
 
   final _api = locator<ApiService>();
   final _hiveDbService = locator<HiveDbService>();
 
+  ReactiveValue<PaymentType> _paymentType =
+      ReactiveValue<PaymentType>(paymentTypes[0]);
+
+  PaymentType? get paymentType => _paymentType.value;
+
   Promocode? _promocode;
 
   Promocode? get promocode => _promocode;
+
+  
 
   /// SEARCHES promocodes and GETS first
   Future<void> searchPromocode(String searchText) async {
