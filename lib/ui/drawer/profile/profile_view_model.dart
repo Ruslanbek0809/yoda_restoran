59ea30cb87fc318d31617f778c87c10da610e0e0
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2022-01-09 12:24:20 +0500

    Working on ProfileView and ProfileVM. Part I

--- lib/ui/drawer/profile/profile_view_model.dart
+++ lib/ui/drawer/profile/profile_view_model.dart
@@ -2,12 +2,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:yoda_res/app/app.locator.dart';
+import 'package:yoda_res/app/app.logger.dart';
 import 'package:yoda_res/app/app.router.dart';
 import 'package:yoda_res/models/hive_models/hive_models.dart';
 import 'package:yoda_res/services/services.dart';
 import 'package:yoda_res/utils/utils.dart';
 
 class ProfileViewModel extends BaseViewModel {
+  final log = getLogger('ProfileViewModel');
+
   final _userService = locator<UserService>();
   final _navService = locator<NavigationService>();
   final _hiveDbService = locator<HiveDbService>();
@@ -26,6 +29,73 @@ class ProfileViewModel extends BaseViewModel {
     await _userService.logoutUser();
     await _hiveDbService.clearCart();
     await navToHomeByRemovingAll();
+  }
+
+  String? _name;
+  String? get name => _name;
+
+  String? _birthDate;
+  String? get birthDate => _birthDate;
+
+  String? _gender;
+  String? get gender => _gender;
+
+  String? _email;
+  String? get email => _email;
+
+  /// UPDATES _name
+  String? updateName(String? value) {
+    log.v('updateName value: $value');
+    if (value!.isEmpty) {
+      return 'Ady giriziň';
+    }
+
+    _name = value;
+    notifyListeners();
+  }
+
+  /// UPDATES _birthDate
+  String? updateBirthDate(String? value) {
+    log.v('updateBirthDate value: $value');
+    if (value!.isEmpty) {
+      return 'Doglan senäňizi giriziň';
+    }
+
+    _birthDate = value;
+    notifyListeners();
+  }
+
+  /// UPDATES _gender
+  String? updateGender(String? value) {
+    log.v('updateGender value: $value');
+    if (value!.isEmpty) {
+      return 'Jynsy giriziň';
+    }
+
+    _gender = value;
+    notifyListeners();
+  }
+
+  /// UPDATES _email
+  String? updateEmail(String? value) {
+    log.v('updateEmail value: $value');
+    if (value!.isEmpty) {
+      return 'Elektron poçtaňyzy giriziň';
+    }
+
+    _email = value;
+    notifyListeners();
+  }
+
+  /// ADDS new address
+  Future<void> onAddAddressPressed() async {
+    log.v('onAddAddressPressed()');
+    try {
+      await runBusyFuture(
+          _userService.addAddress(city, street, house, apartment, floor, note));
+    } catch (err) {
+      throw err;
+    }
   }
 
   void navBack() => _navService.back();
