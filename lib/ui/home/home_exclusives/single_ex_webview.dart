29c85966205f49639d5fa7b829e1ef06cdee87d8
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2022-03-12 17:10:55 +0500

    Updated SingleExWebview feature

--- lib/ui/home/home_exclusives/single_ex_webview.dart
+++ lib/ui/home/home_exclusives/single_ex_webview.dart
@@ -1,41 +1,49 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:stacked_hooks/stacked_hooks.dart';
 import 'package:yoda_res/models/models.dart';
 import 'package:yoda_res/utils/utils.dart';
 
-import 'single_ex_view_model.dart';
-
-class SingleExWebview extends HookViewModelWidget<SingleExViewModel> {
+class SingleExWebview extends StatefulWidget {
   final ExclusiveSingle singleEx;
   const SingleExWebview({required this.singleEx, Key? key}) : super(key: key);
 
   @override
-  Widget buildViewModelWidget(BuildContext context, SingleExViewModel model) {
-    model.log.v('singleEx.url!: ${singleEx.url!}');
-    final GlobalKey webViewKey = GlobalKey();
+  State<SingleExWebview> createState() => _SingleExWebviewState();
+}
 
-    InAppWebViewController? webViewController;
+class _SingleExWebviewState extends State<SingleExWebview> {
+  final GlobalKey webViewKey = GlobalKey();
 
-    InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
-      crossPlatform: InAppWebViewOptions(
-        useShouldOverrideUrlLoading: true,
-        mediaPlaybackRequiresUserGesture: false,
-      ),
-      android: AndroidInAppWebViewOptions(
-        useHybridComposition: true,
-      ),
-      ios: IOSInAppWebViewOptions(
-        allowsInlineMediaPlayback: true,
-      ),
-    );
+  InAppWebViewController? webViewController;
 
-    final urlController = useTextEditingController();
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+    crossPlatform: InAppWebViewOptions(
+      useShouldOverrideUrlLoading: true,
+      mediaPlaybackRequiresUserGesture: false,
+    ),
+    android: AndroidInAppWebViewOptions(
+      useHybridComposition: true,
+    ),
+    ios: IOSInAppWebViewOptions(
+      allowsInlineMediaPlayback: true,
+    ),
+  );
 
-    PullToRefreshController pullToRefreshController = PullToRefreshController(
+  late PullToRefreshController pullToRefreshController;
+
+  String url = "";
+
+  double progress = 0;
+
+  final urlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    pullToRefreshController = PullToRefreshController(
       options: PullToRefreshOptions(
         color: AppTheme.MAIN,
       ),
@@ -48,22 +56,28 @@ class SingleExWebview extends HookViewModelWidget<SingleExViewModel> {
         }
       },
     );
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Stack(
           children: [
             InAppWebView(
               key: webViewKey,
-              initialUrlRequest: URLRequest(url: Uri.parse(singleEx.url!)),
+              initialUrlRequest:
+                  URLRequest(url: Uri.parse(widget.singleEx.url!)),
               initialOptions: options,
               pullToRefreshController: pullToRefreshController,
               onWebViewCreated: (controller) {
                 webViewController = controller;
               },
               onLoadStart: (controller, url) {
-                model.onLoadStart(url);
-                urlController.text = model.url;
+                setState(() {
+                  this.url = url.toString();
+                  urlController.text = this.url;
+                });
               },
               androidOnPermissionRequest:
                   (controller, origin, resources) async {
@@ -75,20 +89,24 @@ class SingleExWebview extends HookViewModelWidget<SingleExViewModel> {
                 if (progress == 100) {
                   pullToRefreshController.endRefreshing();
                 }
-                model.onProgressChanged(progress);
-                urlController.text = model.url;
+                setState(() {
+                  this.progress = progress / 100;
+                  urlController.text = this.url;
+                });
               },
               onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                model.onLoadStart(url);
-                urlController.text = model.url;
+                setState(() {
+                  this.url = url.toString();
+                  urlController.text = this.url;
+                });
               },
               onConsoleMessage: (controller, consoleMessage) {
                 print(consoleMessage);
               },
             ),
-            model.progress < 1.0
+            progress < 1.0
                 ? LinearProgressIndicator(
-                    value: model.progress,
+                    value: progress,
                     backgroundColor: AppTheme.MAIN.withOpacity(0.3),
                     color: AppTheme.MAIN,
                   )
@@ -99,3 +117,103 @@ class SingleExWebview extends HookViewModelWidget<SingleExViewModel> {
     );
   }
 }
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_hooks/flutter_hooks.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:stacked_hooks/stacked_hooks.dart';
+// import 'package:yoda_res/models/models.dart';
+// import 'package:yoda_res/utils/utils.dart';
+
+// import 'single_ex_view_model.dart';
+
+// class SingleExWebview extends HookViewModelWidget<SingleExViewModel> {
+//   final ExclusiveSingle singleEx;
+//   const SingleExWebview({required this.singleEx, Key? key}) : super(key: key);
+
+//   @override
+//   Widget buildViewModelWidget(BuildContext context, SingleExViewModel model) {
+//     final GlobalKey webViewKey = GlobalKey();
+
+//     InAppWebViewController? webViewController;
+
+//     InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+//       crossPlatform: InAppWebViewOptions(
+//         useShouldOverrideUrlLoading: true,
+//         mediaPlaybackRequiresUserGesture: false,
+//       ),
+//       android: AndroidInAppWebViewOptions(
+//         useHybridComposition: true,
+//       ),
+//       ios: IOSInAppWebViewOptions(
+//         allowsInlineMediaPlayback: true,
+//       ),
+//     );
+
+//     final urlController = useTextEditingController();
+
+//     PullToRefreshController pullToRefreshController = PullToRefreshController(
+//       options: PullToRefreshOptions(
+//         color: AppTheme.MAIN,
+//       ),
+//       onRefresh: () async {
+//         if (Platform.isAndroid) {
+//           webViewController?.reload();
+//         } else if (Platform.isIOS) {
+//           webViewController?.loadUrl(
+//               urlRequest: URLRequest(url: await webViewController?.getUrl()));
+//         }
+//       },
+//     );
+
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Stack(
+//           children: [
+//             InAppWebView(
+//               key: webViewKey,
+//               initialUrlRequest: URLRequest(url: Uri.parse(singleEx.url!)),
+//               initialOptions: options,
+//               pullToRefreshController: pullToRefreshController,
+//               onWebViewCreated: (controller) {
+//                 webViewController = controller;
+//               },
+//               onLoadStart: (controller, url) {
+//                 model.onLoadStart(url);
+//                 urlController.text = model.url;
+//               },
+//               androidOnPermissionRequest:
+//                   (controller, origin, resources) async {
+//                 return PermissionRequestResponse(
+//                     resources: resources,
+//                     action: PermissionRequestResponseAction.GRANT);
+//               },
+//               onProgressChanged: (controller, progress) {
+//                 if (progress == 100) {
+//                   pullToRefreshController.endRefreshing();
+//                 }
+//                 model.onProgressChanged(progress);
+//                 urlController.text = model.url;
+//               },
+//               onUpdateVisitedHistory: (controller, url, androidIsReload) {
+//                 model.onLoadStart(url);
+//                 urlController.text = model.url;
+//               },
+//               onConsoleMessage: (controller, consoleMessage) {
+//                 print(consoleMessage);
+//               },
+//             ),
+//             model.progress < 1.0
+//                 ? LinearProgressIndicator(
+//                     value: model.progress,
+//                     backgroundColor: AppTheme.MAIN.withOpacity(0.3),
+//                     color: AppTheme.MAIN,
+//                   )
+//                 : SizedBox(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
