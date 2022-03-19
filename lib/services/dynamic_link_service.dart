56ea3baf87581dd6149a77da280e78d2abc33bee
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2022-03-19 11:37:38 +0500

    Completed navigation part of dynamic link feature

--- lib/services/dynamic_link_service.dart
+++ lib/services/dynamic_link_service.dart
@@ -1,22 +1,27 @@
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:stacked_services/stacked_services.dart';
 
+import '../app/app.locator.dart';
 import '../app/app.logger.dart';
+import '../app/app.router.dart';
 import '../models/models.dart';
 
 class DynamicLinkService {
   final log = getLogger('DynamicLinkService');
+  final _navService = locator<NavigationService>();
+
   FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
 
   Future handleDynamicLinks() async {
     log.v('====== DynamicLinkService STARTED ======');
-    // 1. Get the initial dynamic link if the app is opened with a dynamic link
+
+    /// #1 GETS the initial dynamic link if the app is opened with a dynamic link
     final PendingDynamicLinkData? data = await dynamicLinks.getInitialLink();
 
-    // 2. handle link that has been retrieved
+    /// HANDLES clicked dynamic link
     if (data != null) _handleDeepLink(data);
 
-    // 3. Register a link callback to fire if the app is opened up from the background
-    // using a dynamic link.
+    /// #2 GETS dynamic link in Background / Foreground State
     dynamicLinks.onLink.listen((PendingDynamicLinkData? dynamicLinkData) {
       log.v('====== DynamicLinkService Background / Foreground State ======');
       // 3a. handle link that has been retrieved
@@ -29,7 +34,40 @@ class DynamicLinkService {
   void _handleDeepLink(PendingDynamicLinkData? data) {
     log.v('====== DynamicLinkService _handleDeepLink ======');
     final Uri? deepLink = data?.link;
-    print('_handleDeepLink | deeplink: $deepLink');
+    if (deepLink != null) {
+      print('_handleDeepLink | deeplink: $deepLink');
+
+      // Check if deep link path CONTAINS word 'se'
+      var isSingleEx = deepLink.pathSegments.contains('se');
+
+      if (isSingleEx) {
+        print('deepLink.queryParameters: ${deepLink.queryParameters}');
+
+        // get data of single exclusive from deepLink.queryParameters
+        var singleExId = deepLink.queryParameters['id'];
+        var singleExOrder = deepLink.queryParameters['order'];
+        var singleExName = deepLink.queryParameters['name'];
+        var singleExImage = deepLink.queryParameters['image'];
+        var singleExOption = deepLink.queryParameters['option'];
+        var singleExUrl = deepLink.queryParameters['url'];
+
+        if (singleExId != null) {
+          //------------------ NAVIGATION to SingleExclusive ---------------------//
+          _navService.navigateTo(
+            Routes.exclusiveSingleView,
+            arguments: ExclusiveSingleViewArguments(
+                exclusiveSingle: ExclusiveSingle(
+              id: int.parse(singleExId),
+              order: int.parse(singleExOrder!),
+              name: singleExName,
+              image: singleExImage,
+              option: singleExOption,
+              url: singleExUrl,
+            )),
+          );
+        }
+      }
+    }
   }
 
   Future<String> createDynamicLink(
@@ -41,7 +79,8 @@ class DynamicLinkService {
       // The Dynamic Link URI domain. You can view created URIs on your Firebase console
       uriPrefix: 'https://yodarestoran.page.link/',
       // The deep Link passed to your application which you can use to affect change
-      link: Uri.parse('https://yodarestoran.com/se?id=${singleEx.id}'),
+      link: Uri.parse(
+          'https://yodarestoran.com/se?id=${singleEx.id}&order=${singleEx.order}&name=${singleEx.name}&image=${singleEx.image}&option=${singleEx.option}&url=${singleEx.url}'),
       // Android application details needed for opening correct app on device/Play Store
       androidParameters: const AndroidParameters(
         packageName: 'tm.com.restoran.yoda',
