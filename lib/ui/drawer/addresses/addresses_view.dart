ef3ad54a87a9aca7727032e814b89e3b5a478d8c
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2022-03-26 19:03:36 +0500

    Completed swipe delete feature in Addresses

--- lib/ui/drawer/addresses/addresses_view.dart
+++ lib/ui/drawer/addresses/addresses_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:stacked/stacked.dart';
 import 'package:yoda_res/generated/locale_keys.g.dart';
 import 'package:yoda_res/shared/shared.dart';
@@ -21,7 +22,7 @@ class AddressesView extends StatelessWidget {
           return false;
         },
         child: Scaffold(
-          backgroundColor: kcSecondaryLightColor,
+          // backgroundColor: kcSecondaryLightColor,
           appBar: AppBar(
             backgroundColor: kcWhiteColor,
             elevation: 0.5,
@@ -61,93 +62,170 @@ class AddressesView extends StatelessWidget {
                       svg: 'assets/empty_addresses.svg',
                     )
                   : Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 16.w, vertical: 10.h),
-                      child: ListView.builder(
+                      padding: EdgeInsets.fromLTRB(10.w, 10.h, 0.0, 10.h),
+                      child: ListView.separated(
                         physics: BouncingScrollPhysics(),
                         itemCount: model.addresses!.length,
                         itemBuilder: (context, pos) {
-                          return Container(
-                            decoration: BoxDecoration(
-                              color: kcWhiteColor,
-                              borderRadius: AppTheme().radius10,
-                            ),
-                            margin: EdgeInsets.symmetric(vertical: 5.h),
-                            padding:
-                                EdgeInsets.fromLTRB(12.w, 12.h, 12.w, 10.h),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(bottom: 12.h),
-                                  child: Text(
-                                    model.addresses![pos].street! +
-                                        (model.addresses![pos].house != null
-                                            ? ', ${model.addresses![pos].house}'
-                                            : ''),
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 3,
-                                    style: ktsDefault16Text,
+                          return ClipRRect(
+                            borderRadius: BorderRadius.circular(6.r),
+                            child: Slidable(
+                              // Specify a key if the Slidable is dismissible.
+                              key: ValueKey(model.addresses![pos].id),
+
+                              // The start action pane is the one at the left or the top side.
+                              startActionPane: ActionPane(
+                                dragDismissible: false,
+                                extentRatio: 0.25,
+                                // A motion is a widget used to control how the pane animates.
+                                motion: const ScrollMotion(),
+
+                                // // A pane can dismiss the Slidable.
+                                // dismissible: DismissiblePane(onDismissed: () {}),
+
+                                // All actions are defined in the children parameter.
+                                children: [
+                                  // A SlidableAction can have an icon and/or a label.
+                                  SlidableAction(
+                                    onPressed: (BuildContext context) {},
+                                    backgroundColor: kcRedColor,
+                                    foregroundColor: kcWhiteColor,
+                                    icon: Icons.delete_outline_rounded,
+                                  ),
+                                ],
+                              ),
+
+                              // The child of the Slidable is what the user sees when the
+                              // component is not dragged.
+                              child: GestureDetector(
+                                onTap: () {},
+                                child: Padding(
+                                  padding:
+                                      EdgeInsets.fromLTRB(6.w, 5.h, 16.w, 5.h),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Flexible(
+                                        child: Text(
+                                          model.addresses![pos].street! +
+                                              (model.addresses![pos].house !=
+                                                      null
+                                                  ? ', ${model.addresses![pos].house}'
+                                                  : ''),
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: kts18Text,
+                                        ),
+                                      ),
+                                      SizedBox(width: 5.w),
+                                      Icon(
+                                        Icons.arrow_forward_ios_rounded,
+                                        color: kcSecondaryDarkColor,
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Expanded(
-                                      child: CustomIconTextButton(
-                                        icon: Icon(
-                                          Icons.edit_outlined,
-                                          color: kcSecondaryDarkColor,
-                                        ),
-                                        text: Text(
-                                          'Üýtget',
-                                          style: kts16DarkText,
-                                        ),
-                                        backgroundColor: kcSecondaryLightColor,
-                                        onPressed: () {},
-                                      ),
-                                    ),
-                                    SizedBox(width: 10.w),
-                                    Expanded(
-                                      child: CustomIconTextButton(
-                                        icon: Icon(
-                                          Icons.delete_outline_rounded,
-                                          color: kcSecondaryDarkColor,
-                                        ),
-                                        text: Text(
-                                          'Poz',
-                                          style: kts16DarkText,
-                                        ),
-                                        backgroundColor: kcSecondaryLightColor,
-                                        onPressed: () async =>
-                                            await model.showAddressRemoveDialog(
-                                                model, model.addresses![pos]),
-                                      ),
-                                    ),
-                                    SizedBox(width: 10.w),
-                                    Expanded(
-                                      child: CustomIconTextButton(
-                                        icon: Icon(
-                                          Icons.check_circle_outline_rounded,
-                                          color: kcSecondaryDarkColor,
-                                        ),
-                                        text: Text(
-                                          'Saýla',
-                                          style: kts16DarkText,
-                                        ),
-                                        backgroundColor: kcSecondaryLightColor,
-                                        onPressed: () {},
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ],
+                              ),
                             ),
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.only(left: 6.w),
+                            child: Divider(color: kcDividerColor),
                           );
                         },
                       ),
                     ),
+          // Padding(
+          //   padding: EdgeInsets.symmetric(
+          //       horizontal: 16.w, vertical: 10.h),
+          //   child: ListView.builder(
+          //     physics: BouncingScrollPhysics(),
+          //     itemCount: model.addresses!.length,
+          //     itemBuilder: (context, pos) {
+          //       return Container(
+          //         decoration: BoxDecoration(
+          //           color: kcWhiteColor,
+          //           borderRadius: AppTheme().radius10,
+          //         ),
+          //         margin: EdgeInsets.symmetric(vertical: 5.h),
+          //         padding:
+          //             EdgeInsets.fromLTRB(12.w, 12.h, 12.w, 10.h),
+          //         child: Column(
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //             Padding(
+          //               padding: EdgeInsets.only(bottom: 12.h),
+          //               child: Text(
+          //                 model.addresses![pos].street! +
+          //                     (model.addresses![pos].house != null
+          //                         ? ', ${model.addresses![pos].house}'
+          //                         : ''),
+          //                 overflow: TextOverflow.ellipsis,
+          //                 maxLines: 3,
+          //                 style: ktsDefault16Text,
+          //               ),
+          //             ),
+          //             Row(
+          //               mainAxisAlignment:
+          //                   MainAxisAlignment.spaceBetween,
+          //               children: [
+          //                 Expanded(
+          //                   child: CustomIconTextButton(
+          //                     icon: Icon(
+          //                       Icons.edit_outlined,
+          //                       color: kcSecondaryDarkColor,
+          //                     ),
+          //                     text: Text(
+          //                       'Üýtget',
+          //                       style: kts16DarkText,
+          //                     ),
+          //                     backgroundColor: kcSecondaryLightColor,
+          //                     onPressed: () {},
+          //                   ),
+          //                 ),
+          //                 SizedBox(width: 10.w),
+          //                 Expanded(
+          //                   child: CustomIconTextButton(
+          //                     icon: Icon(
+          //                       Icons.delete_outline_rounded,
+          //                       color: kcSecondaryDarkColor,
+          //                     ),
+          //                     text: Text(
+          //                       'Poz',
+          //                       style: kts16DarkText,
+          //                     ),
+          //                     backgroundColor: kcSecondaryLightColor,
+          //                     onPressed: () async =>
+          //                         await model.showAddressRemoveDialog(
+          //                             model, model.addresses![pos]),
+          //                   ),
+          //                 ),
+          //                 SizedBox(width: 10.w),
+          //                 Expanded(
+          //                   child: CustomIconTextButton(
+          //                     icon: Icon(
+          //                       Icons.check_circle_outline_rounded,
+          //                       color: kcSecondaryDarkColor,
+          //                     ),
+          //                     text: Text(
+          //                       'Saýla',
+          //                       style: kts16DarkText,
+          //                     ),
+          //                     backgroundColor: kcSecondaryLightColor,
+          //                     onPressed: () {},
+          //                   ),
+          //                 ),
+          //               ],
+          //             )
+          //           ],
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // ),
         ),
       ),
       viewModelBuilder: () => AddressesViewModel(),
