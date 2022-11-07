7874f1a4fb32f9a3b5056a901da57ee862f281e4
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2022-11-07 10:59:51 +0500

    Working on postOnlinePayment part VII

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -612,18 +612,46 @@ class UserService {
     try {
       //----------- DIO PART START -------------//
       Dio dio = Dio();
-      Map<String, dynamic> _headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      };
+      // Map<String, dynamic> _headers = {
+      //   'Accept': 'application/json',
+      //   'Content-Type': 'application/json',
+      // };
 
       dio.options.baseUrl = 'https://mpi.gov.tm/payment/rest/register.do';
-      dio.options.headers = _headers;
+      // dio.options.headers = _headers;
 
+      dio.interceptors.add(
+        InterceptorsWrapper(
+          onRequest: (options, handler) {
+            // Do something before request is sent
+            log.v(
+                'REQUEST[${options.method}] => PATH: ${options.baseUrl}?${options.queryParameters} OR ${options.data}');
+            return handler.next(options); //continue
+            // If you want to resolve the request with some custom data，
+            // you can resolve a `Response` object eg: `handler.resolve(response)`.
+            // If you want to reject the request with a error message,f
+            // you can reject a `DioError` object eg: `handler.reject(dioError)`
+          },
+          onResponse: (response, handler) {
+            // Do something with response data
+            return handler.next(response); // continue
+            // If you want to reject the request with a error message,
+            // you can reject a `DioError` object eg: `handler.reject(dioError)`
+          },
+          onError: (DioError e, handler) {
+            // Do something with response error
+            return handler.next(e); //continue
+            // If you want to resolve the request with some custom data，
+            // you can resolve a `Response` object eg: `handler.resolve(response)`.
+          },
+        ),
+      );
       //----------- DIO PART END -------------//
 
       Response response = await dio.post(
         '',
+        // '?userName=Ver43k764ghwS2H&password=101211004240&orderNumber=${order.orderNumber}&amount=$_totalOrderSum&returnUrl=https://mpi.gov.tm/payment/finish.html&currency=934&language=ru&pageView=DESKTOP&clientId=${_currentUser!.id}',
+        // queryParameters: _queryParams,
         data: onlinePaymentFormData,
       );
       log.v('RESPONSE: postOnlinePayment => ${response.data}');
