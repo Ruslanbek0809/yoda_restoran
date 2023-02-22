b63d5ae1dbb87896158af3a156ebd9a2accfed39
Ruslan Hasanov <78399117+Ruslanbek0809@users.noreply.github.com>
2023-02-22 17:05:24 +0500

    Updated app version to 2.7.1+47

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
